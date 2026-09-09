1f6e8cc4cdc37ed705795df1918c5d694e47ed8b
DHRUV JAIN <121154592+dhruv-4444@users.noreply.github.com>
2026-09-09 16:19:23 +0530

    Time: 6 ms (27.77%), Space: 48.2 MB (6.93%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,37 +1,29 @@
 class Solution {
     public int islandPerimeter(int[][] grid) {
-        int[] delRow = {-1, 0, 1, 0};
-        int[] delCol = {0, 1, 0, -1};
+        int[] delRow = { -1, 0, 1, 0 };
+        int[] delCol = { 0, 1, 0, -1 };
         int n = grid.length;
         int m = grid[0].length;
         int perimeter = 0;
         int check = 0;
-        boolean[][] vis = new boolean[n][m];
+        boolean vis[][] = new boolean[n][m];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-
                 if (grid[i][j] == 1) {
                     vis[i][j] = true;
-                    check = 0;
-
+                     check = 0;
                     for (int k = 0; k < 4; k++) {
-
+                       
                         int row = i + delRow[k];
                         int col = j + delCol[k];
-
-                        if (row < 0 || row >= n || 
-                            col < 0 || col >= m ||
-                            grid[row][col] == 0) {
-
+                        if (row < 0 || row >= n || col < 0 || col >= m || grid[row][col] == 0) {
                             check++;
                         }
                     }
-
                     perimeter += check;
                 }
             }
         }
-
         return perimeter;
     }
 }
